58d041fb498301f1b9a12de5629bee203fc071a7
Sharon-Liang <sliang@iphy.ac.cn>
2021-11-19 12:13:46 +0800

    D = 8 and D = 16 data

--- plot.jl
+++ plot.jl
@@ -58,99 +58,90 @@ function chi_div_w(w::Vector, path::String, n::Int64)
 end
 
 
-"""load Zi-Long Li data"""
-g = 2.0
+"""Nevanlinna"""
+omega = [i for i in range(-4π,4π,step = π/400)]
+#load Zi-Long Li data
+g = 1.0
 (d,r) = divrem(g, 1)
 invT = [10, 20, 30, 40]
 N = 40
 
-plc = Vector{String}(undef, length(invT))
-oplc = Vector{String}(undef, length(invT))
+plca = Vector{String}(undef, length(invT))
+plcs = Vector{String}(undef, length(invT))
 for i = 1:length(invT)
-    plc[i] = @sprintf "./data/TFIsing-Li/imaginary_time_data/g_%ip%i_beta_%i_tau.txt" d 10*r invT[i]
-    oplc[i] = @sprintf "./data/TFIsing-Li/imaginary_time_data/g_%ip%i_beta_%i_iwn.txt" d 10*r invT[i]
+    plca[i] = @sprintf "./data/TFIsing-Li/g_%ip%i_beta_%i_A.txt" d 10*r invT[i]
+    plcs[i] = @sprintf "./data/TFIsing-Li/g_%ip%i_beta_%i_S.txt" d 10*r invT[i]
 end
 
-for i = 1:length(invT)
-    open(oplc[i], "w") do file
-        for n = 1:N
-            d = readdlm(plc[i])
-            res = giwn(n,invT[i],d[:,1],d[:,2])
-            writedlm(file, [Masubara_freq(n,invT[i]) real(res) imag(res)])
-        end
-    end
-end
-        
-
-
-
-
-"""setups"""
-omega = [i for i in range(-4π,4π,step = π/400)]
-
-"""Nevanlinna"""
-pcollect = Vector{String}(undef, length(invT))
-for i = 1:length(invT)
-    pcollect[i] = @sprintf "./data/gt/gt28_g_%.1f_b_%i.txt" g invT[i]
+#load ls data
+D = [8, 16]
+pc = Matrix{String}(undef, length(invT), 2*length(D))
+for i = 1:length(invT), j=1:length(D)
+    pc[i,2j-1] = @sprintf "./data/imagtime/gdivwn_g_%.1f_D_%i_beta_%i.txt" g D[j] invT[i]
+    pc[i,2j] = @sprintf "./data/imagtime/gdivwn_g_%.1f_D_2m%i_beta_%i.txt" g D[j] invT[i]
 end
 
-pcollect2 = Vector{String}(undef, length(invT))
-for i = 1:length(invT)
-    pcollect2[i] = @sprintf "./data/gt/gt8_g_%.1f_b_%i.txt" g invT[i]
-end
-
-dc = Vector{Vector}(undef,length(invT))
-dc2 = Vector{Vector}(undef,length(invT))
+dc = Matrix{Vector}(undef, length(invT), 2*length(D))
 dl = Vector{Matrix}(undef,length(invT))
+
+n = 4
 for i = 1:length(invT)
-    dc[i] = chi_div_w(omega, pcollect[i], 5)
-    dc2[i] = chi_div_w(omega, pcollect2[i], 5)
     dl[i] = chi_div_w(plca[i])
     d = readdlm(plcs[i]); bs0 = invT[i]*d[800,2]
     dl[i][800,2] = bs0
+    for j=1:2*length(D)
+        dc[i,j] = chi_div_w(omega, pc[i,j], n)
+    end
 end
 
-"""sum rule check"""
-dω = π/400
-for i = 1:4
-    s1 = sum(dc[i])* dω 
-    s2 = sum(dc2[i])* dω
-    str1 = @sprintf "β=%i, D=16, sum = %.5f" invT[i] s1
-    str2 = @sprintf "β=%i, D=8, sum = %.5f" invT[i] s2
-    println(str1)
-    println(str2)
-end
-
-for i = 1:4
-    s = sum(dl[i])* dω 
-    str = @sprintf "β=%i, sum = %.5f" invT[i] s
-    println(str)
-end
+β = 20
+t = div(β,10)
 
 
 
-β = 20; num = div(β, 10) |> Int
 
+#g!=1.0
 plot()
 v = 2 * abs(g-1)
 plot!([-v,v], seriestype="vline",line=(:dash, :black), label=false)
-plot!(dl[num][:,1], dl[num][:,2], line=(:orange, 2),label="Li")
-plot!(omega, dc[num],line=(:dash, 2),label="cmpo:n=5")
+plot!(dl[t][:,1], dl[t][:,2], line=(:solid, 2),label="Li")
+plot!(omega, dc[t,1],line=(:dash, 2),label="χ=8")
+plot!(omega, dc[t,2],line=(:dash, 2),label="χ=8×2")
+plot!(omega, dc[t,3],line=(:dash, 2),label="χ=16")
+plot!(omega, dc[t,4],line=(:dash, 2),label="χ=16×2")
 plot!(xlim=(-2π, 2π))
-plot!(title = @sprintf "g = %.1f, β=20, η=0.001" g)
+#plot!(xlim=(0,2π), ylim=(0, maximum(de)*1.1))
+plot!(title = @sprintf "g=%.1f, β=%i, η=0.001, n=%i" g β n)
 plot!(xlabel="ω", ylabel="χ''(ω)/ω")
-spath = @sprintf "./notes/chi_g_%.1f_b20.pdf" g
+spath = @sprintf "./notes/chi_g_%.1f_b%i_n%i.pdf" g invT[t] n
 savefig(spath)
 
 
+#g=1.0
+de = [spectral_density(1.0, i, β) for i in omega]
+plot(omega, de ./omega, line=(:black,2),label="theory")
+plot!(dl[t][:,1], dl[t][:,2], line=(:solid, 2),label="Li")
+plot!(omega, dc[t,1],line=(:dash, 2),label="χ=8")
+plot!(omega, dc[t,2],line=(:dash, 2),label="χ=8×2")
+plot!(omega, dc[t,3],line=(:dash, 2),label="χ=16")
+plot!(omega, dc[t,4],line=(:dash, 2),label="χ=16×2")
+plot!(xlim=(-0.25, 0.25))
+#plot!(xlim=(0,2π), ylim=(0, maximum(de)*1.1))
+plot!(title = @sprintf "g=%.1f, β=%i, η=0.001, n=%i" g β n)
+plot!(xlabel="ω", ylabel="χ''(ω)/ω")
+spath = @sprintf "./notes/chi_g_%.1f_b%i_n%i.pdf" g invT[t] n
+savefig(spath)
+
+
+#nchange
 nrange = [3,5,7,9]
 li = [:dash, :solid]
 c = palette(:tab20c)
 dn = Vector{Vector}(undef,length(nrange))
 for i = 1:length(nrange)
-    dn[i] = chi_div_w(omega, pcollect[num], nrange[i])
+    dn[i] = chi_div_w(omega, pc[t,], nrange[i])
 end
-plot!(dl[num][:,1], dl[num][:,2], line=(:black, 2),label="Li")
+plot!(dl[t][:,1], dl[t][:,2], line=(:black, 2),label="Li")
 for j=1:length(nrange)
     plot!(omega, dn[j], line=(li[rem(j,2)+1], c[nrange[j]],1.5), 
     label=@sprintf "n=%i" nrange[j])
@@ -160,6 +151,8 @@ plot!(title = @sprintf "g = %.1f, β=20, η=0.001" g)
 plot!(xlabel="ω", ylabel="χ''(ω)/ω")
 spath = @sprintf "./notes/chi_g_%.1f_b20_nchange.pdf" g
 savefig(spath)
+
+
 
 
 nrange = [4,5,6,7]
@@ -187,33 +180,124 @@ savefig(spath)
 
 
 
-"""
-de = [spectral_density(1.0, i, β) for i in omega]
-plot!(omega, de ./omega, line=(:black,2),label="theory")
-plot!(dl[num][:,1], dl[num][:,2], line=(:dash,:orange, 2),label="Li")
-plot!(omega, dc[num],line=(:dash, 2),label="cmpo:n=5")
-plot!(xlim=(-1,1))
-#plot!(xlim=(0,2π), ylim=(0, maximum(de)*1.1))
-plot!(title = @sprintf "g = %.1f, β = %i, η=0.001" g β)
-plot!(xlabel="ω", ylabel="χ''(ω)/ω")
-spath = @sprintf "./notes/chi_g_%.1f_b20.pdf" g
-savefig(spath)
+
+
+"""sum rule check"""
+dω = π/400
+for i = 1:4
+    s1 = sum(dc[i])* dω 
+    s2 = sum(dc2[i])* dω
+    str1 = @sprintf "β=%i, D=16, sum = %.5f" invT[i] s1
+    str2 = @sprintf "β=%i, D=8, sum = %.5f" invT[i] s2
+    println(str1)
+    println(str2)
+end
+
+
+
+"""Check integral data"""
+inputs = readdlm("./data/imagtime/gtau_g_1.0_D_8_beta_20.txt")
+outputs = [giwn(i,20,inputs[:,1].*20,inputs[:,2]) for i = 1:40]
+anly = readdlm("./data/imagtime/giwn_g_1.0_D_8_beta_20.txt")
 
 c = palette(:okabe_ito)
-for j=1:4
-    plot!(omega, dc[j], line=(1.7), label=@sprintf "cmpo:β=%i" invT[j])
+scatter(anly[:,1], anly[:,2] .- real.(outputs), line=(:dash, c[5], 1), 
+    marker=(:circle, 6, c[5], stroke(1.5,c[5],1.0)), 
+    label="analytic")
+scatter!(anly[:,1], real.(outputs), 
+    marker=(:circle, 6, :white, stroke(1.5,c[6],1.0)), 
+    label="integral")
+plot!(xlabel="ωn", ylabel="Re ΔG(iωn)", title="g=1.0, β=20, χ=8",
+    legend=:bottomright)
+savefig("./notes/check_integral_real_diff.pdf")
+
+
+
+"""load Zi-Long Li data"""
+g = 1.0
+(d,r) = divrem(g, 1)
+invT = [10, 20, 30, 40]
+N = 40
+
+plc = Vector{String}(undef, length(invT))
+oplc = Vector{String}(undef, length(invT))
+for i = 1:length(invT)
+    plc[i] = @sprintf "./data/TFIsing-Li/imaginary_time_data/g_%ip%i_beta_%i_tau.txt" d 10*r invT[i]
+    oplc[i] = @sprintf "./data/TFIsing-Li/imaginary_time_data/g_%ip%i_beta_%i_iwn.txt" d 10*r invT[i]
 end
-for j=1:4
-    plot!(dl[j][:,1], dl[j][:,2], line=(:dash, c[4+j],1), 
-    marker=(:circle, 3, :white, stroke(1,c[4+j],1.0)), 
-    label=@sprintf "Li:β=%i" invT[j])
+
+"""load ls data"""
+D = [8, 16]
+pc = Matrix{String}(undef, length(invT), 2*length(D))
+opc = Matrix{String}(undef, length(invT), 2*length(D))
+for i = 1:length(invT), j=1:length(D)
+    pc[i,2j-1] = @sprintf "./data/imagtime/gtau_g_%.1f_D_%i_beta_%i.txt" g D[j] invT[i]
+    pc[i,2j] = @sprintf "./data/imagtime/gtau_g_%.1f_D_2m%i_beta_%i.txt" g D[j] invT[i]
+    opc[i,2j-1] = @sprintf "./data/imagtime/giwn_g_%.1f_D_%i_beta_%i.txt" g D[j] invT[i]
+    opc[i,2j] = @sprintf "./data/imagtime/giwn_g_%.1f_D_2m%i_beta_%i.txt" g D[j] invT[i]
 end
-plot!(xlim=(-0.5, 0.5))
-plot!(title = @sprintf "g = %.1f, cmpo n=5, η=0.001" g)
-plot!(xlabel="ω", ylabel="χ''(ω)/ω")
-spath = @sprintf "./notes/chi_g_%.1f.pdf" g
+
+"""compare G(iωn)"""
+β = 40
+t=div(β,10)
+
+dl = readdlm(oplc[t])
+dc = [readdlm(opc[t,i]) for i=1:2*length(D)]
+
+c = palette(:okabe_ito)
+#scatter(dl[:,1], dl[:,2] , line=(:dash, :black, 1), 
+#    marker=(:circle, 6, :black, stroke(1.5,:black,1.0)), 
+#    label="Li")
+scatter(dc[1][:,1], dc[1][:,2] .- dl[:,2], 
+    marker=(:circle, 6, stroke(0.5,1.0)), 
+    label="χ=8")
+scatter!(dc[2][:,1], dc[2][:,2] .- dl[:,2], 
+    marker=(:circle, 6, stroke(0.5,1.0)), 
+    label="χ=8×2")
+scatter!(dc[3][:,1], dc[3][:,2] .- dl[:,2], 
+    marker=(:circle, 6, stroke(0.5,1.0)), 
+    label="χ=16")
+scatter!(dc[4][:,1], dc[4][:,2] .- dl[:,2], 
+    marker=(:circle, 6, stroke(0.5,1.0)), 
+    label="χ=16×2")
+plot!(xlabel="ωn", ylabel="|Re(ΔG(iωn))|", title="g=1.0, β=20, χ=8",
+    legend=:bottomright)
+savefig("./notes/giwn_real_diff.pdf")
+
+
+
+
+"""compare G(τ)"""
+β = 40
+t=div(β,10)
+
+dl = readdlm(plc[t])
+dc = [readdlm(pc[t,i]) for i=1:2*length(D)]
+
+for i = 1:2*length(D)
+    dc[i][1:1600,2] = abs.(dc[i][1:1600,2] .- dl[:,2])
+end
+
+plot(dc[1][1:1600,1],dc[1][1:1600,2], line=(2),label="χ=8")
+plot!(dc[2][1:1600,1],dc[2][1:1600,2], line=(2),label="χ=2×8")
+plot!(dc[3][1:1600,1],dc[3][1:1600,2], line=(2),label="χ=16")
+plot!(dc[4][1:1600,1],dc[4][1:1600,2], line=(2),label="χ=2×16")
+ptitle = @sprintf "G(τ) difference of g=%.1f, at β=%i" g invT[t]
+plot!(xlabel="τ/β", ylabel="|ΔG(τ)|", title=ptitle)
+spath = @sprintf "./notes/gtau_diff_g_%.1f_beta_%i.pdf" g invT[t]
 savefig(spath)
-"""
+
+  
+
+
+
+
+
+
+
+
+
+
 
 """sum rule check"""
 
