855a36bbe803776bcfbf44b258dfa41c8f57c956
Sharon-Liang <sliang@iphy.ac.cn>
2021-12-30 23:36:49 +0800

    update

--- test/PickMatrix.jl
+++ test/PickMatrix.jl
@@ -1,5 +1,5 @@
 using nevanlinna_ac, Test
-using Printf
+using DoubleFloats
 
 pcollect = Vector{String}(undef, 2)
 pcollect[1] = "./data/gaussian/giwn_gaussian_1.txt"
@@ -12,6 +12,18 @@ for p in pcollect, n = 1:40
     a = all( abs.(ϕ) .≤ 1 )
     b, _= isGeneralizedSchursovable(xn, yn, tolerance = 0.)
     a && b ? continue : 
+        println("")
+        println(p, ", n = ", n)
+        println("   pick matrix positive semidefinite: ", b, "; |ϕ|≤ 1: ", a)
+end
+
+for p in pcollect, n = 1:40 
+    x, y = readGF(p, num = n)
+    xn, yn = toGeneralizedSchurdata(x, y, :f)
+    ϕ = schur_parameter(Double64, xn, yn)
+    a = all( abs.(ϕ) .≤ 1 )
+    b, _= isGeneralizedSchursovable(xn, yn, tolerance = 0.)
+    a == b ? continue : 
         println("")
         println(p, ", n = ", n)
         println("   pick matrix positive semidefinite: ", b, "; |ϕ|≤ 1: ", a)
