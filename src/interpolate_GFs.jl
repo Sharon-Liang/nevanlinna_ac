ce6ff54980e827384897fc9f34ab8dddc7f1df5e
Sharon-Liang <sliang@iphy.ac.cn>
2022-10-25 11:17:41 +0800

    feat: add G(0) constrain

--- src/interpolate_GFs.jl
+++ src/interpolate_GFs.jl
@@ -47,12 +47,16 @@ end
 
 Calculate the spectral function `A(ω)` for given dataset `{x=ωn, y=G(iωn)}` at `ω`.
 """
-function spectral_function_value(ω::Number, operator_type::OperatorType, x::AbstractVector, y::AbstractVector, float_type::DataType=Double64; η::Real = 1.e-5, toreverse::Bool=true, init_func::Function= z -> zero(Complex{float_type}))
+function spectral_function_value(ω::Number, operator_type::OperatorType, x::AbstractVector, y::AbstractVector, C0::Real, float_type::DataType=Double64; η::Real = 1.e-5, toreverse::Bool=true, init_func::Function= z -> zero(Complex{float_type}))
     x, y = toNevanlinnadata(x, y, operator_type, float_type)
     if toreverse == true
         x = reverse(x)
         y = reverse(y)
     end
+    C0 = convert(float_type, C0)
+    append!(x, one(float_type)im * η)
+    append!(y, η*(one(float_type) - η)*C0)
+
     if isNevanlinnasolvable(x,y)[1] == false @warn "Nevanlinna unsolvable!" end
     
     ω = convert(float_type, ω)
@@ -73,9 +77,9 @@ end
 
 Calculate the spectral function `A(ω)` for given dataset `{x=ωn, y=G(iωn)}` in the range `[-ωmax, ωmax)` with `Nω` discrete values.
 """
-function spectral_function(operator_type::OperatorType, x::AbstractVector, y::AbstractVector, float_type::DataType=Double64; ωmax::Real = 4π, Nω::Integer = 500, η::Real = 1.e-5, toreverse::Bool=true, init_func::Function= z -> zero(Complex{float_type}))
+function spectral_function(operator_type::OperatorType, x::AbstractVector, y::AbstractVector, C0::Real, float_type::DataType=Double64; ωmax::Real = 4π, Nω::Integer = 500, η::Real = 1.e-5, toreverse::Bool=true, init_func::Function= z -> zero(Complex{float_type}))
     L = 2*ωmax
     ωlist = convert(Vector{float_type}, (-Nω/2:Nω/2-1)*L/Nω)
-    Alist = map(ω -> spectral_function_value(ω, operator_type, x, y, float_type; η, toreverse, init_func), ωlist)
+    Alist = map(ω -> spectral_function_value(ω, operator_type, x, y, C0, float_type; η, toreverse, init_func), ωlist)
     return ωlist, Alist
 end
