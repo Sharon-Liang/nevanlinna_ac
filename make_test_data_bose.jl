3cedf8ee617fd65c4b5e8593a03bd450ea870361
Sharon-Liang <sliang@iphy.ac.cn>
2022-11-11 14:38:15 +0800

    feat: new test data

--- make_test_data_bose.jl
+++ make_test_data_bose.jl
@@ -10,11 +10,11 @@ end
 
 
 """
-two_pole_model_GF(z, a1, a2, e1, e2)
+two_pole_model(z, a1, a2, e1, e2)
 
     Green's Function for two pole model
 """
-function two_pole_model_GF(z, a1=0.1, a2=0.7, e1=0.7, e2=2.5)
+function two_pole_model(z, a1=0.1, a2=0.7, e1=0.7, e2=2.5)
     return a1/(z^2 - e1^2) + a2/(z^2 - e2^2)
 end
 
@@ -29,6 +29,7 @@ function _fermi_distribution(e, β = 1.0, μ = -0.5)
     return 1.0/den
 end
 
+
 """
 _energy_density(px, py)
 
@@ -41,7 +42,7 @@ _energy_density(px, py) =  -0.5*(cos(px) + cos(py))
 hubbard_model_rpa()
     RPA result of Masubara frequency GF for doppend Hubbard model, energy unit D = 4t =1
 """
-function _gf_kernel(z, qx, qy, px, py, β = 1.0, μ = -0.5)
+function _hubbard_gf_kernel(z, qx, qy, px, py, β = 1.0, μ = -0.5)
     ep = _energy_density(px, py)
     epq = _energy_density(px+qx, py+qy)
 
@@ -55,43 +56,74 @@ function _gf_kernel(z, qx, qy, px, py, β = 1.0, μ = -0.5)
     return res
 end
 
-function hubbard_model_rpa(z, qx, qy)
-    f = p -> _gf_kernel(z, qx, qy, p[1], p[2])/(2π)^2
+function hubbard_model_rpa(z, qx, qy, β=1.0, μ=-0.5)
+    f = p -> _hubbard_gf_kernel(z, qx, qy, p[1], p[2], β, μ)/(2π)^2
     return hcubature(f, [-π, -π], [π, π])[1]
 end
 
 
-#Generate Test Data
-Nf = 100
-β = 1
-ωnlist = [Masubara_freq(n, β, Bose) for n=0:Nf]
-Giωnlist = map(z->hubbard_model_rpa(1.0im * z, π, π), ωnlist)
-
-gfile = "./data/giwn_hubbard_M_point_noise.txt"
-open(gfile, "w") do file
-    write(file, "        ωn                   Re.G(iωn)           Im.G(iωn)      \n")
-    write(file, "--------------------  --------------------  -------------------- \n")
-    #writedlm(file, [ωnlist real.(Giωnlist) imag.(Giωnlist)])
-
-    writedlm(file, [ωnlist real.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist)) imag.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist))])
+"""
+band_gap_model()
+    RPA result of Masubara frequency GF for band_gap_model, energy unit D = 4t =1
+"""
+function _band_gap_gf_kernel(z, qx, qy, px, py, β = 1.0, μ = 1.7)
+    ep_val = _energy_density(px, py) 
+    epq_val = _energy_density(px+qx, py+qy)
+    res = 0.
+    for ep in [ep_val, ep_val + 3.], epq in [epq_val, epq_val + 3.]
+        if abs(z) == 0. && abs(ep - epq) <= 1.e-10
+            res += -β*exp((ep-μ)*β)/ _fermi_distribution(ep, β, μ)^2
+        else
+            num = _fermi_distribution(ep, β, μ) - _fermi_distribution(epq, β, μ)
+            den = ep - epq + z
+            res += num/den
+        end
+    end
+    return res
 end
 
+function band_gap_model(z, qx, qy, β=1.0, μ=1.7)
+    f = p -> _band_gap_gf_kernel(z, qx, qy, p[1], p[2], β, μ)/(2π)^2
+    return hcubature(f, [-π, -π], [π, π])[1]
+end
+
+#Generate Test Data
+β = 1
+
+p1 = 0
+p2 = π/2
 
 ωlist = [i for i in range(-2π, 2π, length=500)]
 η = 0.05
-Alist = map(z->-2*imag(hubbard_model_rpa(z + 1.0im * η, π, π)), ωlist)
+Alist = map(z->-2*imag(two_pole_model(z + 1.0im * η)), ωlist)
+
+plot(ωlist, Alist)
 
 
-afile = "./data/Aw_hubbard_M_point.txt"
+
+afile = @sprintf "./data/Aw_two_pole_model_beta_%.2f.txt" β
 open(afile, "w") do file
-    write(file, "        ωn                      A(ω)      \n")
+    write(file, "        ω                       A(ω)      \n")
     write(file, "--------------------  --------------------\n")
     writedlm(file, [ωlist Alist])
 end
 
 
+Nf = 100
+ωnlist = [Masubara_freq(n, β, Bose) for n=0:Nf]
+Giωnlist = map(z->two_pole_model(1.0im * z), ωnlist)
 
-f, _, _ = multi_gaussian(3, μrange=[1,3])
+gfile = @sprintf "./data/giwn_two_pole_model_beta_%.2f.txt" β
+open(gfile, "w") do file
+    write(file, "        ωn                   Re.G(iωn)           Im.G(iωn)      \n")
+    write(file, "--------------------  --------------------  -------------------- \n")
+    writedlm(file, [ωnlist real.(Giωnlist) imag.(Giωnlist)])
+
+    #writedlm(file, [ωnlist real.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist)) imag.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist))])
+end
+
+
+
 
 
 
