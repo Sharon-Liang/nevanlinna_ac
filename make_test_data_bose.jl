57bc6a57ca38a859fd9189c2d371c92f0aa81506
Sharon-Liang <sliang@iphy.ac.cn>
2022-11-15 11:55:22 +0800

    fix: rpa

--- make_test_data_bose.jl
+++ make_test_data_bose.jl
@@ -1,5 +1,5 @@
 using Random; Random.seed!()
-using DelimitedFiles, Printf, Plots
+using DelimitedFiles, Printf
 using HCubature, LinearAlgebra
 using NevanlinnaAC: OperatorType, Bose, Fermi
 
@@ -24,9 +24,27 @@ _fermi_distribution(e, β, μ = 0.5)
     
     Fermi distribution function
 """
-function _fermi_distribution(e, β = 1.0, μ = -0.5)
+function _fermi_distribution(e, β, μ = 0.)
     den = exp((e-μ)*β) + 1
     return 1.0/den
+end
+
+
+"""
+_rpa_kernel(z, e1, e2, β, μ)
+
+    _rpa_kernel = [nF(e1) - nF(e2)]/[z + e1 - e2] where `nF(e)` is the _fermi_distribution function
+"""
+function _rpa_kernel(z, e1, e2, β, μ)
+    if abs(z) == 0. && abs(e1 - e2) <= 1.e-10
+        ϵ = (e1-μ)*β/2
+        res = -β * sech(ϵ)^2 /4
+    else
+        num = _fermi_distribution(e1, β, μ) - _fermi_distribution(e2, β, μ)
+        den = e1 - e2 + z
+        res = num/den
+    end
+    return res
 end
 
 
@@ -38,95 +56,40 @@ _energy_density(px, py)
 _energy_density(px, py) =  -0.5*(cos(px) + cos(py))
 
 
+
 """
-hubbard_model_rpa()
+hubbard_model(z, qx, qy, β=1.0, μ=-0.5)
     RPA result of Masubara frequency GF for doppend Hubbard model, energy unit D = 4t =1
 """
-function _hubbard_gf_kernel(z, qx, qy, px, py, β = 1.0, μ = -0.5)
+function _hubbard_gf_kernel(z, qx, qy, px, py, β, μ)
     ep = _energy_density(px, py)
     epq = _energy_density(px+qx, py+qy)
-
-    num = _fermi_distribution(ep, β, μ) - _fermi_distribution(epq, β, μ)
-    den = ep - epq + z
-    if abs(z) == 0. && abs(ep - epq) <= 1.e-10
-        res = -β*exp((ep-μ)*β)/ _fermi_distribution(ep, β, μ)^2
-    else
-        res = num/den
-    end
-    return res
+    res = _rpa_kernel(z, ep, epq, β, μ)
+    return convert(typeof(z), res)
 end
 
-function hubbard_model_rpa(z, qx, qy, β=1.0, μ=-0.5)
+function hubbard_model(z, qx, qy, β=1.0, μ=-0.5)
     f = p -> _hubbard_gf_kernel(z, qx, qy, p[1], p[2], β, μ)/(2π)^2
     return hcubature(f, [-π, -π], [π, π])[1]
 end
 
 
 """
-band_gap_model()
-    RPA result of Masubara frequency GF for band_gap_model, energy unit D = 4t =1
+bandgap_model(z, qx, qy, β=1.0, μ=1.7, eshift=3.)
+    RPA result of Masubara frequency GF for bandgap_model, energy unit D = 4t =1
 """
-function _band_gap_gf_kernel(z, qx, qy, px, py, β = 1.0, μ = 1.7)
-    ep_val = _energy_density(px, py) 
-    epq_val = _energy_density(px+qx, py+qy)
+function _band_gap_gf_kernel(z, qx, qy, px, py, β, μ, eshift)
+    ep = _energy_density(px, py) 
+    epq = _energy_density(px+qx, py+qy)
     res = 0.
-    for ep in [ep_val, ep_val + 3.], epq in [epq_val, epq_val + 3.]
-        if abs(z) == 0. && abs(ep - epq) <= 1.e-10
-            res += -β*exp((ep-μ)*β)/ _fermi_distribution(ep, β, μ)^2
-        else
-            num = _fermi_distribution(ep, β, μ) - _fermi_distribution(epq, β, μ)
-            den = ep - epq + z
-            res += num/den
-        end
+    for e1 in [ep, ep + eshift], e2 in [epq, epq + eshift]
+        res += _rpa_kernel(z, e1, e2, β, μ)
     end
     return res
 end
 
-function band_gap_model(z, qx, qy, β=1.0, μ=1.7)
-    f = p -> _band_gap_gf_kernel(z, qx, qy, p[1], p[2], β, μ)/(2π)^2
+function bandgap_model(z, qx, qy, β=1.0, μ=1.7, eshift=3.)
+    f = p -> _band_gap_gf_kernel(z, qx, qy, p[1], p[2], β, μ, eshift)/(2π)^2
     return hcubature(f, [-π, -π], [π, π])[1]
 end
-
-#Generate Test Data
-β = 1
-
-p1 = 0
-p2 = π/2
-
-ωlist = [i for i in range(-2π, 2π, length=500)]
-η = 0.05
-Alist = map(z->-2*imag(two_pole_model(z + 1.0im * η)), ωlist)
-
-plot(ωlist, Alist)
-
-
-
-afile = @sprintf "./data/Aw_two_pole_model_beta_%.2f.txt" β
-open(afile, "w") do file
-    write(file, "        ω                       A(ω)      \n")
-    write(file, "--------------------  --------------------\n")
-    writedlm(file, [ωlist Alist])
-end
-
-
-Nf = 100
-ωnlist = [Masubara_freq(n, β, Bose) for n=0:Nf]
-Giωnlist = map(z->two_pole_model(1.0im * z), ωnlist)
-
-gfile = @sprintf "./data/giwn_two_pole_model_beta_%.2f.txt" β
-open(gfile, "w") do file
-    write(file, "        ωn                   Re.G(iωn)           Im.G(iωn)      \n")
-    write(file, "--------------------  --------------------  -------------------- \n")
-    writedlm(file, [ωnlist real.(Giωnlist) imag.(Giωnlist)])
-
-    #writedlm(file, [ωnlist real.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist)) imag.(Giωnlist).+ 1.e-8 .* rand(length(Giωnlist))])
-end
-
-
-
-
-
-
-
-
 
